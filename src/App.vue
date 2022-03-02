1335c785b1ea80bf82b9755f4f7053dde189989b
citlalli <citlalli.sanchez2890@alumnos.udg.mx>
2022-03-01 18:35:37 -0600

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+    
+  }
+   
+}
+
+
+</script>
+<template>
+  <img alt="Vue logo" src="./assets/logocj.jpg" width="300">
+  <HelloWorld msg="BIENVENIDO A CREACIONES JUDITH"/>
+
+  <h1>{{ message }}</h1>
+
+  <!--
+    Bind to a method/function.
+    The @click syntax is short for v-on:click.
+  -->
+  <button @click="reverseMessage">Reverse Message</button>
+
+  <!-- Can also be an inline expression statement -->
+  <button @click="message += '!'">Append "!"</button>
+ 
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button, a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      En esta pagina podras obtener todos los enlaces a nuestras redes, para que no te pierdas de las actualizacioines.
+      
+    </p>
+    <h3>redes</h3>
+    <ul>
+      <li><a href="https://instagram.com/creaciones.judith.78?utm_medium=copy_link">instagram</a></li>
+      <li><a href="">Facebook</a></li>
+      <li><a href="">Whatsapp</a></li>
+    </ul>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
